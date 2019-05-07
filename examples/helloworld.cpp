cda2f350e0c9c13151c54563aea85dca968eb9ad
He Listen <lswmistlsw@163.com>
2019-05-07 22:03:54 +0800

    Update helloworld.cpp

--- examples/helloworld.cpp
+++ examples/helloworld.cpp
@@ -1,13 +1,14 @@
 #include "crow.h"
-
+#include <time.h>
 int main()
 {
     crow::SimpleApp app;
 
     CROW_ROUTE(app, "/")
     ([]() {
+        usleep(2863)
         return "Hello world!";
     });
 
-    app.port(18080).run();
+    app.port(10080).multithreaded.run();
 }
